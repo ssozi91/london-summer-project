256bb36c9caf7b5c090925c7c13ed4b51163695f
Frank Ssozi <raymossekandi@gmail.com>
2017-08-19 12:04:45 +0100

    served public folder

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 
 var app = express();
+app.use(express.static('public'))
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
